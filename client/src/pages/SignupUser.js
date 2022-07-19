cf62b4b20ee16841aea47b8d720419a3d44a0f62
munjehun <miso1489@gmail.com>
2022-07-19 14:10:58 +0900

    회원가입 기능 통합, 회원가입 아이디 중복체크 구현

--- client/src/pages/SignupUser.js
+++ client/src/pages/SignupUser.js
@@ -1,43 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignupUser.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function SignupUser() {
+  const navigate = useNavigate();
+
+  const [Id, setId] = useState("");
+  const [Password, setPassword] = useState("");
+  const [PasswordConfirm, setPasswordConfirm] = useState("");
+
+  const onIdHandler = (event) => {
+    setId(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onPasswordConfirmHandler = (event) => {
+    setPasswordConfirm(event.currentTarget.value);
+  };
+
+  //회원가입 요청
+  const onSubmitHandler = () => {
+    if (Id.length === 0 || Password.length === 0) {
+      alert("ID와 비밀번호를 모두 입력해주세요");
+      return;
+    }
+    let body = {
+      user_id: Id,
+      user_pass: Password,
+    };
+
+    axios
+      .request({
+        method: "POST",
+        url: "https://localhost:4000/api/user/join",
+        data: body,
+        withCredentials: true,
+      })
+      .then((res) => {
+        const user = res.data.data; //회원가입하면 받는 유저 정보
+        console.log(user);
+
+        //로그인 완료 후 메인으로 가기
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //중복검사
+  const onDuplicateCheckHandler = () => {
+    let body = {
+      user_id: Id,
+    };
+    axios
+      .request({
+        method: "POST",
+        url: "https://localhost:4000/api/user/checkId",
+        data: { user_id: Id },
+        withCredentials: true,
+      })
+      .then((res) => {
+        const data = res.data.message;
+        if (data == "already in use") {
+          alert("이미 사용중인 아이디입니다.");
+        } else {
+          alert("사용 가능한 아이디입니다.");
+        }
+      });
+  };
   return (
     <div className="signup_user">
       <div className="form_container">
         <div className="title">회원 가입</div>
         <div className="inputs">
           <div>
+            {/* 아이디 입력 칸 */}
             <input
               type="text"
-              // value={Username}
-              // onChange={onUsernameHandler}
+              value={Id}
+              onChange={onIdHandler}
               placeholder="ID"
-              // onKeyPress={onKeyPress}
             />
-            <button>중복 체크</button>
+            <button onClick={onDuplicateCheckHandler}>중복 체크</button>
           </div>
-
+          {/* 비밀번호 입력 칸 */}
           <input
             type="password"
-            // value={Password}
-            // onChange={onPasswordHandler}
+            value={Password}
+            onChange={onPasswordHandler}
             placeholder="Password"
-            // onKeyPress={onKeyPress}
           />
+          {/* 비밀번호 확인 입력 칸 */}
           <input
             type="password"
-            // value={Password}
-            // onChange={onPasswordHandler}
+            value={PasswordConfirm}
+            onChange={onPasswordConfirmHandler}
             placeholder="Confirm Password "
-            // onKeyPress={onKeyPress}
           />
         </div>
+        {/* 회원가입 신청 버튼 */}
         <div className="submit">
-          <button
-            type="submit"
-            //   onClick={onSubmitHandler}
-          >
+          <button type="submit" onClick={onSubmitHandler}>
             가입하기
           </button>
         </div>
