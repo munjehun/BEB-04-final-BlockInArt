eaf7c540dd9790daa78c5b0fe071b070c572b619
dnwlaos23 <dnwlaos23@gmail.com>
2022-07-21 17:18:57 +0900

    ipfs 함수 추가

--- server/controller/web3/createNFT.js
+++ server/controller/web3/createNFT.js
@@ -1,25 +1,39 @@
 const abi = require("./abi");
 const contractAddr = require("./contractAddr");
 
+const fs = require("fs");
+
 const Caver = require("caver-js");
-const caver = new Caver('https://api.baobab.klaytn.net:8651/')
+const caver = new Caver("https://api.baobab.klaytn.net:8651/");
+
+const ipfsUpload = require("./ipfsUpload");
 
 module.exports = {
   post: async (req, res) => {
     try {
 
-      const account = await caver.klay.getAccount();
+      const json = JSON.stringify({
+        desc: 'test',
+        name: 'test',
+      })
+
+      const jUrl = await ipfsUpload(json);
+
+
       const tokenContract = await new caver.klay.Contract(abi, contractAddr, {
-        from: '0x505c94083EB4DAe6a2f92cE485a3335F631Be77C'
+        from: '0x505c94083EB4DAe6a2f92cE485a3335F631Be77C',
       });
       tokenContract.options.address = contractAddr;
 
-      const newTokenId = await tokenContract.methods.mintNFT('');
+      // const newTokenId = await tokenContract.methods.mintNFT(jUrl).send({
+      //   from: '0x505c94083EB4DAe6a2f92cE485a3335F631Be77C',
+      //   gas: 2000000,
+      // });
 
-      console.log(account);
+      // console.log(account);
       console.log(newTokenId);
 
-      res.send("test");
+      res.send(ipfs);
     } catch (error) {
       console.log(error);
     }
---
+++ server/controller/web3/ipfsUpload.js
@@ -0,0 +1,21 @@
+const IpfsApi = require("ipfs-api");
+const ipfs = IpfsApi("ipfs.infura.io", "5001", { protocol: "https" });
+
+const fs = require("fs");
+
+async function ipfsUpload(json) {
+
+  const data = Buffer.from(json)
+
+  const jsonAdded = await ipfs.add(data);
+
+  const path = jsonAdded[0].path;
+
+  const jUrl = `https://ipfs.infura.io/ipfs/${path}`;
+
+  console.log(jUrl);
+
+  return jUrl;
+}
+
+module.exports = ipfsUpload;
