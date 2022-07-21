c809cb5e41c6ad3e608dcfa945666810a9568dcb
wnsghchl <ghwns5668@naver.com>
2022-07-21 14:27:54 +0900

    작품등록 , 작품목록 api 완성

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -2,18 +2,14 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-function ProductCard() {
+function ProductCard({ picture_name, img, price, artist }) {
   return (
     <div>
-      <div className="picture_name">작품명</div>
-      <img
-        width="100%"
-        height="100%"
-        src="https://phinf.pstatic.net/dbscthumb/3329_000_9/20170315200826092_ZYKBVY02G.jpg/9750605_i1.jpg?type=m2000_2000_fst"
-      ></img>
+      <div className="picture_name">{picture_name}</div>
+      <img width="100%" height="100%" src={img}></img>
       <div className="price_painter">
-        <div>가격</div>
-        <div className="작가명">작가명</div>
+        <div>{price}원</div>
+        <div className="작가명">{artist}</div>
       </div>
       <Link to="detailPainter">
         <Button className="container__detail-btn" variant="outline-primary">
