bd9d2af7ebb76d36fd3fe8d7ec0e89cb7135b49e
munjehun <miso1489@gmail.com>
2022-07-26 16:13:56 +0900

    trade_state가 3일때도 계약 진행,취소 페이지로 이동 구현

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,8 +1,9 @@
 export default function Loading() {
   return (
     <div className="d-flex justify-content-center">
+      <div>NFT가 발급되는 중입니다&nbsp;&nbsp;</div>
       <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
+        <div> </div>
       </div>
     </div>
   );
